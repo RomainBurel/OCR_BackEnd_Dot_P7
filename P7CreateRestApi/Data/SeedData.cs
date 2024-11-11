6a2a30e5e9d2bc3549a12fd46512bd92a6c74060
Romain BUREL <generic3@diademe.fr>
2024-11-11 19:27:22 +0100

    feat: refactor SeedData in order to use public constants

--- P7CreateRestApi/Data/SeedData.cs
+++ P7CreateRestApi/Data/SeedData.cs
@@ -5,19 +5,25 @@ namespace Dot.Net.WebApi.Data
 {
     public static class SeedData
     {
+        public const string ADMIN_MAIL = "admin@findexium.com";
+        public const string USER_MAIL = "user@findexium.com";
+        public const string ADMIN_PWD = "Admin123@";
+        public const string USER_PWD = "User1234@";
         const string ADMIN_ROLE_NAME = "Admin";
         const string USER_ROLE_NAME = "User";
 
         public static async Task SeedAdminUserAsync(IServiceProvider serviceProvider)
         {
-            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
-            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var db = serviceProvider.GetRequiredService<LocalDbContext>();
+            db.Database.EnsureCreated();
 
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             await SeedData.AddRole(roleManager, ADMIN_ROLE_NAME);
             await SeedData.AddRole(roleManager, USER_ROLE_NAME);
 
-            await SeedData.AddUser(userManager, "AdminUser", "admin@findexium.com", "Admin123@", ADMIN_ROLE_NAME);
-            await SeedData.AddUser(userManager, "SimpleUser", "user@findexium.com", "User1234@", USER_ROLE_NAME);
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            await SeedData.AddUser(userManager, "AdminUser", ADMIN_MAIL, ADMIN_PWD, ADMIN_ROLE_NAME);
+            await SeedData.AddUser(userManager, "SimpleUser", USER_MAIL, USER_PWD, USER_ROLE_NAME);
         }
 
         private static async Task AddRole(RoleManager<IdentityRole> roleManager, string role)
