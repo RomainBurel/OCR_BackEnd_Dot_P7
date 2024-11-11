4bc0cb6bc7357ab08d962fc81f3bf1fe07826233
Romain BUREL <generic3@diademe.fr>
2024-11-11 19:25:36 +0100

    feat: remove useless usings + add Authorize to RatingController

--- P7CreateRestApi/Controllers/RatingController.cs
+++ P7CreateRestApi/Controllers/RatingController.cs
@@ -1,4 +1,3 @@
-using Dot.Net.WebApi.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using P7CreateRestApi.Models;
@@ -6,6 +5,7 @@ using P7CreateRestApi.Services;
 
 namespace Dot.Net.WebApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class RatingController : ControllerBase
