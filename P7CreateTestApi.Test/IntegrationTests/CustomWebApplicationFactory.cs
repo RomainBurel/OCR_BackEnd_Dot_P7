68a59a45f9722fdd0164bf63d3fdbacd71038206
Romain BUREL <generic3@diademe.fr>
2024-11-11 19:39:48 +0100

    feat: test - prepare integration tests with CustomWebApplicationFactory

---
+++ P7CreateTestApi.Test/IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,102 @@
+﻿using Dot.Net.WebApi.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using P7CreateRestApi.Models;
+using P7CreateRestApi.Repositories;
+using P7CreateRestApi.Services;
+using System.Data.Common;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace P7CreateTestApi.Test.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<LocalDbContext>));
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+
+                var dbConnectionDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection));
+                if (dbConnectionDescriptor != null)
+                {
+                    services.Remove(dbConnectionDescriptor);
+                }
+
+                services.AddDbContext<LocalDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase($"TestDB");
+                });
+
+                services.AddScoped<IBidListRepository, BidListRepository>();
+                services.AddScoped<ICurvePointRepository, CurvePointRepository>();
+                services.AddScoped<IRatingRepository, RatingRepository>();
+                services.AddScoped<IRuleNameRepository, RuleNameRepository>();
+                services.AddScoped<ITradeRepository, TradeRepository>();
+                services.AddScoped<IUserRepository, UserRepository>();
+
+                services.AddScoped<IBidListService, BidListService>();
+                services.AddScoped<ICurvePointService, CurvePointService>();
+                services.AddScoped<IRatingService, RatingService>();
+                services.AddScoped<IRuleNameService, RuleNameService>();
+                services.AddScoped<ITradeService, TradeService>();
+                services.AddScoped<IUserService, UserService>();
+
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                {
+                    SeedData.SeedAdminUserAsync(scope.ServiceProvider).GetAwaiter().GetResult();
+                }
+            });
+
+            builder.UseEnvironment("Development");
+        }
+
+        public async Task LoginAsAdmin(HttpClient httpClient)
+        {
+            await AuthenticateUserAsync(httpClient, SeedData.ADMIN_MAIL, SeedData.ADMIN_PWD);
+        }
+
+        public async Task LoginAsUser(HttpClient httpClient)
+        {
+            await AuthenticateUserAsync(httpClient, SeedData.USER_MAIL, SeedData.USER_PWD);
+        }
+
+        public void LogoutUser(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = null; 
+        }
+
+        private async Task AuthenticateUserAsync(HttpClient httpClient, string email, string pwd)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetJwtAsync(httpClient, email, pwd));
+        }
+
+        private async Task<string> GetJwtAsync(HttpClient httpClient, string email, string pwd)
+        {
+            var loginModel = new LoginModel
+            {
+                Email = email,
+                Password = pwd
+            };
+
+            var response = await httpClient.PostAsJsonAsync("/Login/login", loginModel);
+            response.EnsureSuccessStatusCode();
+
+            var registrationResponse = await response.Content.ReadAsStringAsync();
+            using (var document = JsonDocument.Parse(registrationResponse))
+            {
+                var json = document.RootElement.GetProperty("token").GetString();
+                return json;
+            }
+        }
+    }
+}
