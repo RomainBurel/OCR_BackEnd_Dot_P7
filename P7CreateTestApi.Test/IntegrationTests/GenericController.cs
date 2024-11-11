26db1af527362d667ab13d42e2097bd806358302
Romain BUREL <generic3@diademe.fr>
2024-11-11 19:40:43 +0100

    feat: test - add GenericController for integration tests + Ratingontroller partial tests

---
+++ P7CreateTestApi.Test/IntegrationTests/GenericController.cs
@@ -0,0 +1,29 @@
+﻿using Dot.Net.WebApi.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace P7CreateTestApi.Test.IntegrationTests
+{
+    public class GenericController<T> : IClassFixture<CustomWebApplicationFactory<Program>> where T : class
+    {
+        protected readonly CustomWebApplicationFactory<Program> _factory;
+        protected readonly HttpClient _httpClient;
+
+        public GenericController(CustomWebApplicationFactory<Program> factory)
+        {
+            this._factory = factory;
+            this._httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions() { AllowAutoRedirect = false });
+        }
+
+        protected async Task ClearDatabase()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<LocalDbContext>();
+                var dbSet = context.Set<T>();
+                context.RemoveRange(dbSet.ToArray());
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ P7CreateTestApi.Test/IntegrationTests/RatingControllerTest.cs
@@ -0,0 +1,96 @@
+﻿using Dot.Net.WebApi.Domain;
+using P7CreateRestApi.Models;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace P7CreateTestApi.Test.IntegrationTests
+{
+    public class RatingControllerTest: GenericController<Rating>
+    {
+        public RatingControllerTest(CustomWebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        private List<Rating> GetRating()
+        {
+            return new List<Rating> {
+                new Rating() { Id = 1, MoodysRating = "Moodys1", SandPRating = "Sand1", FitchRating = "Fitch1" },
+                new Rating() { Id = 2, MoodysRating = "Moodys2", SandPRating = "Sand4", FitchRating = "Fitch2" },
+                new Rating() { Id = 3, MoodysRating = "Moodys3", SandPRating = "Sand3", FitchRating = "Fitch3" },
+                new Rating() { Id = 4, MoodysRating = "Moodys4", SandPRating = "Sand4", FitchRating = "Fitch4" }
+            };
+        }
+
+        private async Task SeedSampleRatingsAsync()
+        {
+            await this.ClearDatabase();
+            await this._factory.LoginAsAdmin(this._httpClient);
+
+            var ratings = GetRating();
+            foreach (var rating in ratings)
+            {
+                await this._httpClient.PostAsJsonAsync("/Rating/creation", new RatingModelAdd
+                {
+                    MoodysRating = rating.MoodysRating,
+                    SandPRating = rating.SandPRating,
+                    FitchRating = rating.FitchRating,
+                    OrderNumber = rating.OrderNumber
+                });
+            }
+
+            this._factory.LogoutUser(_httpClient);
+        }
+
+        [Fact]
+        public async Task GetAll_AsLoggedUser_ShouldReturn_AllRecords()
+        {
+            // Arrange
+            await this.SeedSampleRatingsAsync();
+            await _factory.LoginAsUser(this._httpClient);
+
+            // Act
+            var response = await this._httpClient.GetAsync("/Rating/list");
+            var ratings = await response.Content.ReadFromJsonAsync<List<RatingModel>>();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(ratings);
+            Assert.Equal(this.GetRating().Count, ratings.Count);
+        }
+
+        [Fact]
+        public async Task GetAll_NoLoggedUser_ShouldReturn_Unauthorized()
+        {
+            // Arrange
+            this._factory.LogoutUser(this._httpClient);
+
+            // Act
+            var response = await this._httpClient.GetAsync("/Rating/list");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetRatingById_AsLoggedUser_ShouldReturn_Record()
+        {
+            // Arrange
+            await this.SeedSampleRatingsAsync();
+            await _factory.LoginAsUser(this._httpClient);
+            var expectedRating = this.GetRating()[0];
+
+            // Act
+            var response = await this._httpClient.GetAsync($"/Rating/display/{expectedRating.Id}");
+            var rating = await response.Content.ReadFromJsonAsync<RatingModel>();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(rating);
+            Assert.Equal(expectedRating.Id, rating.Id);
+            Assert.Equal(expectedRating.MoodysRating, rating.MoodysRating);
+            Assert.Equal(expectedRating.SandPRating, rating.SandPRating);
+            Assert.Equal(expectedRating.FitchRating, rating.FitchRating);
+            Assert.Equal(expectedRating.OrderNumber, rating.OrderNumber);
+        }
+    }
+}
